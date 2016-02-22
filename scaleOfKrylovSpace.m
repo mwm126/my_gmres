1f67a637efaedf12ed1b58ecf09ebbc576371b8c
Mark Meredith <mwm126@gmail.com>
2016-02-22 09:53:45 -0500

    use single instead of double quotes (matlab is pickier than octave)

--- scaleOfKrylovSpace.m
+++ scaleOfKrylovSpace.m
@@ -1,7 +1,7 @@
 % Octave: run as octave -qf main.m
 % Matlab: delete last line ("main") and run
 
-function main
+function scaleOfKrylovSpace
 
   % nn = 100;
   % npd_AA = sprand(nn,nn,7.0/nn); % npd_AA is not positive definite
@@ -12,13 +12,13 @@ function main
   % fscanf(fd,'%f')
   % fclose(fd)
 
-  
-  printf("Loading AA matrix\n");
+
+  fprintf('Loading AA matrix\n');
   load AA.dat
-  printf("Converting AA matrix\n");
+  fprintf('Converting AA matrix\n');
   AA = spconvert(AA);
-  printf("AA Matrix ready\n");
-  
+  fprintf('AA Matrix ready\n');
+
   fd = fopen('bb.dat','r')
   bb = fscanf(fd,'%f');
   fclose(fd);
@@ -33,49 +33,49 @@ function main
   display(size(xx))
   display(size(bb))
 
-  printf("Building Krylov space\n");
-  
+  fprintf('Building Krylov space\n');
+
   MM = inv(diag(diag(AA)));
-  
+
   %display (MM)
-  
+
   x = krylovSpaceNorms( AA, xx, bb, MM, 12);
 
 end
 
 function [x] = krylovSpaceNorms ( A, x, b, M, n )
-    
+
       t = M*x;
-      
+
 %      display(t)
-    
+
     k{1} = b - A*t;
 
-    
-    printf ( '%d, %e\n',1,norm(k{1}));
+
+    fprintf ( '%d, %e\n',1,norm(k{1}));
     for i = 2:n,
         t = M*k{i-1};
-    
+
         k{i} = A*t;
-        printf( '%d, %e\n',i,norm(k{i}));
+        fprintf( '%d, %e\n',i,norm(k{i}));
     end
-    
-    display("Building dot product matrix\n");
-    
+
+    display('Building dot product matrix\n');
+
     for i = 1:n,
         for j = 1:n,
             N(i,j) = dot(k{i},k{j});
         end
     end
-    
+
     display (N)
-    
-    display ("Det(N):")
+
+    display ('Det(N):')
     display (det(N))
-    
+
     invN = inv(N)
-    
-    
+
+
 end
 
-main
+scaleOfKrylovSpace
