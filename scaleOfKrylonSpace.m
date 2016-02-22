ed7938fcfd6604e78f9ff415cec16a06f33df7f9
mprinkey <mprinkey@users.noreply.github.com>
2016-02-22 09:42:12 -0500

    Added files via upload
    Simple test to generate naive Krylov space and test linear independence of the basis.

---
+++ scaleOfKrylonSpace.m
@@ -0,0 +1,81 @@
+% Octave: run as octave -qf main.m
+% Matlab: delete last line ("main") and run
+
+function main
+
+  % nn = 100;
+  % npd_AA = sprand(nn,nn,7.0/nn); % npd_AA is not positive definite
+  % AA = npd_AA'*npd_AA; % AA is positive definite
+  % bb = rand(nn,1);
+
+  % fd = fopen('AA.txt','r')
+  % fscanf(fd,'%f')
+  % fclose(fd)
+
+  
+  printf("Loading AA matrix\n");
+  load AA.dat
+  printf("Converting AA matrix\n");
+  AA = spconvert(AA);
+  printf("AA Matrix ready\n");
+  
+  fd = fopen('bb.dat','r')
+  bb = fscanf(fd,'%f');
+  fclose(fd);
+
+  xx = zeros(size(bb,1),1);
+  MM = speye(size(bb,1));
+  rr = 20;
+  mit = 10000;
+  toller = 1.0e-4;
+
+  display(size(AA))
+  display(size(xx))
+  display(size(bb))
+
+  printf("Building Krylov space\n");
+  
+  MM = inv(diag(diag(AA)));
+  
+  %display (MM)
+  
+  x = krylovSpaceNorms( AA, xx, bb, MM, 12);
+
+end
+
+function [x] = krylovSpaceNorms ( A, x, b, M, n )
+    
+      t = M*x;
+      
+%      display(t)
+    
+    k{1} = b - A*t;
+
+    
+    printf ( '%d, %e\n',1,norm(k{1}));
+    for i = 2:n,
+        t = M*k{i-1};
+    
+        k{i} = A*t;
+        printf( '%d, %e\n',i,norm(k{i}));
+    end
+    
+    display("Building dot product matrix\n");
+    
+    for i = 1:n,
+        for j = 1:n,
+            N(i,j) = dot(k{i},k{j});
+        end
+    end
+    
+    display (N)
+    
+    display ("Det(N):")
+    display (det(N))
+    
+    invN = inv(N)
+    
+    
+end
+
+main
